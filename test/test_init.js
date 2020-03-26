ea1034862f2158d11c4fe9771a746ddd1010fb4e
Yaroslav <y.surilov@infomir.com>
2020-03-26 20:06:26 +0200

    cleanup project from old files

--- test/test_init.js
+++
@@ -1,5 +0,0 @@
-describe('Initialization tests', function () {
-	it('Library itself should be an object', function () {
-		expect(Object.prototype.toString.call(zJS)).toBe('[object Object]');
-	});
-});
